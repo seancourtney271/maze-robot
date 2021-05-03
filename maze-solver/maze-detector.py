f3211e4c742ffe7c3d0fc61532369c5935b4b169
kodywebe <kodytweber@gmail.com>
2021-05-03 18:07:56 -0400

    mnk

--- maze-solver/maze-detector.py
+++ maze-solver/maze-detector.py
@@ -18,12 +18,36 @@ ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
 
 # define the criteria to stop and refine the corners
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-#corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
-
+corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+centroids = centroids[1:]
+corners = corners[1:]
+#print(centroids)
 # Now draw them
 res = np.hstack((centroids,corners))
 res = np.int0(res)
-img[res[:,1],res[:,0]]=[0,0,255]
+#img[res[:,1],res[:,0]]=[0,0,255]
 img[res[:,3],res[:,2]] = [0,255,0]
+
+
+
+
+#for index in range(1,len(corners)-1):
+#    #print(corners[index+1])
+#    #print(corners[index])
+#    dist = np.subtract(corners[index+1],corners[index])
+#    #print(dist)
+#    
+#    dist = dist**2
+#    #print(dist)
+#    
+#    dist = np.sqrt(np.sum(dist))
+#    #print(dist)
+#    #print("--------")
+#    print(corners[index])
+#    if(dist <= 20):
+#        print(int(corners[index][0]))
+#        print(int(corners[index][1]))
+#        img[int(corners[index][0]),int(corners[index][1])] = [255,0,0]
+#        img[int(corners[index+1][0]),int(corners[index+1][1])] = [255,0,0]
 
 cv2.imwrite('subpixel5.png',img)
